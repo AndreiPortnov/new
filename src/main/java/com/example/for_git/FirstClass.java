6a79ad9ff6aaa7287180aec59bce0afe6e7eb90d
andrei portnov <a.portnov163@gmail.com>
2023-06-06 12:27:46 +0400

    add second field in FirstClass & create SecondClass

--- src/main/java/com/example/for_git/FirstClass.java
+++ src/main/java/com/example/for_git/FirstClass.java
@@ -3,4 +3,5 @@ package com.example.for_git;
 public class FirstClass {
 
     private Long id = 7L;
+    private Long idw = 7L;
 }
---
+++ src/main/java/com/example/for_git/SecondClass.java
@@ -0,0 +1,4 @@
+package com.example.for_git;
+
+public class SecondClass {
+}
