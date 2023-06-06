2db2dee29467d0282548eb84f1db8b450974369a
andrei portnov <a.portnov163@gmail.com>
2023-06-06 10:46:24 +0400

    initial commit

---
+++ src/test/java/com/example/for_git/ForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.for_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
